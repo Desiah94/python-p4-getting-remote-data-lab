608cc117f718e14fc7c5ad54f228ab55ad51d536
Desiah Barnett <desiahbarnett@gmail.com>
2024-03-17 21:50:09 -0400

    completed lab

--- lib/GetRequester.py
+++ lib/GetRequester.py
@@ -1,13 +1,18 @@
 import requests
 import json
 
-class GetRequester:
+import requests
 
+class GetRequester:
+    
     def __init__(self, url):
         self.url = url
 
     def get_response_body(self):
-        pass
+        response = requests.get(self.url)
+        return response.content  # Return content as bytes
 
     def load_json(self):
-        pass
+        response_body = self.get_response_body()
+        decoded_response = response_body.decode('utf-8')  # Decode bytes to string
+        return json.loads(decoded_response)  # Parse JSON from string
